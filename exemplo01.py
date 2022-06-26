89601b4a00d68eaa0dbc8f5ecf1c6a13953fb165
EandroC <eandrocleiton@gmail.com>
2022-06-26 07:23:22 -0300

    Introdução ao Python

---
+++ exemplo01.py
@@ -0,0 +1,10 @@
+# Mostre na tela o comando "Hello world"
+print("Hello World")
+
+# Mostre na tela o resultado da soma de 30 + 50
+print(30+50)
+
+#Mostre o conteúdo da variavel soma de números
+soma_de_numeros = 30 + 50
+print(soma_de_numeros)
+
